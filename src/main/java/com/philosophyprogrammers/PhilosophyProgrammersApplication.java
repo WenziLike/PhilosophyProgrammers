de5eb0f9ff0625fb1f71cbedd828d33b1b2f0c27
WenziLike <SlavaMyraxin@icloud.com>
2021-10-21 00:37:30 +0200

    add Application plus test Application

---
+++ src/main/java/com/philosophyprogrammers/PhilosophyProgrammersApplication.java
@@ -0,0 +1,13 @@
+package com.philosophyprogrammers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilosophyProgrammersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhilosophyProgrammersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/philosophyprogrammers/PhilosophyProgrammersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philosophyprogrammers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilosophyProgrammersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
