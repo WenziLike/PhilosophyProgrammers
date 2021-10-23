5369475313d70a1399270fd0b82b130e4062b95a
WenziLike <SlavaMyraxin@icloud.com>
2021-10-23 12:57:48 +0200

    deleted

--- src/main/java/com/philosophyprogrammers/service/CrudService.java
+++
@@ -1,9 +0,0 @@
-package com.philosophyprogrammers.service;
-
-import com.philosophyprogrammers.entity.User;
-
-public interface CrudService {
-    User registerNewUser(User account);
-
-    void saveRegisteredUser(User user);
-}
