7a6b8a4917638207bbc3b15d92e805c584e75c23
WenziLike <SlavaMyraxin@icloud.com>
2021-11-07 22:16:26 +0100

    adding UnknownIdentifierException

---
+++ src/main/java/com/philosophyprogrammers/exceptions/UnknownIdentifierException.java
@@ -0,0 +1,15 @@
+package com.philosophyprogrammers.exceptions;
+
+public class UnknownIdentifierException extends Exception {
+    public UnknownIdentifierException() {
+        super();
+    }
+
+    public UnknownIdentifierException(String message) {
+        super(message);
+    }
+
+    public UnknownIdentifierException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
