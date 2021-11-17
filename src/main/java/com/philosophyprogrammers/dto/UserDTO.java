9493bea7a73b7d17baaf5c0a6fcbbc980e88dfc8
WenziLike <SlavaMyraxin@icloud.com>
2021-11-17 23:50:21 +0100

    adding lombok

--- src/main/java/com/philosophyprogrammers/dto/UserDTO.java
+++ src/main/java/com/philosophyprogrammers/dto/UserDTO.java
@@ -1,5 +1,8 @@
 package com.philosophyprogrammers.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.*;
 import java.io.Serializable;
 
@@ -10,6 +13,8 @@ import java.io.Serializable;
  * @version 1.0
  */
 
+@Getter
+@Setter
 public class UserDTO implements Serializable {
 
     @NotEmpty(message = "First Name can not be empty")
@@ -25,64 +30,4 @@ public class UserDTO implements Serializable {
     private String password;
     @NotEmpty(message = "Password can not be empty")
     private String confirmPassword;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-
-    @Override
-    public String toString() {
-        return "UserDTO{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", confirmPassword='" + confirmPassword + '\'' +
-                '}';
-    }
 }
