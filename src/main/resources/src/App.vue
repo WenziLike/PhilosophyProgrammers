4e692c474849fe0b2e4b73f435acd65e87d4d34d
WenziLike <SlavaMyraxin@icloud.com>
2021-11-20 00:53:52 +0100

    add VueJS 3

---
+++ src/main/resources/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Starting</h1>
+  <router-view to="/home"></router-view>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
