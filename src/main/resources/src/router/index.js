805a6c95b9f504e7711596f07bdd3926ba1f3197
WenziLike <SlavaMyraxin@icloud.com>
2021-11-23 22:39:30 +0100

    add path pages

--- src/main/resources/src/router/index.js
+++ src/main/resources/src/router/index.js
@@ -1,25 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: () => import('@/views/Register')
+    },
+    {
+        path: '/write',
+        name: 'writeStory',
+        component: () => import('@/views/WriteStory')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+    linkActiveClass: "active",
+    linkExactActiveClass: "active"
 })
 
 export default router
